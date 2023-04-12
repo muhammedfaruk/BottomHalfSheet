888332a7023d793b1d9e57d7014167a889774d3a
Muhammed Faruk <faruk.sogut@btpro.net>
2023-04-12 09:29:32 +0300

    Initial Commit

---
+++ BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Muhammed Faruk Söğüt on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
